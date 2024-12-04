5a43036062a6b5189e207e6995ba133384352e84
Artsiom Kapset <artsiom.kapset@mmc.com>
2024-12-04 15:54:46 +0100

    breadthFirstSearch and depthFirstSearch are added for BST

--- 22 Binary Search Trees/BinarySearchTree.js
+++ 22 Binary Search Trees/BinarySearchTree.js
@@ -55,6 +55,64 @@ class BinarySearchTree {
 
     return found
   }
+
+  breadthFirstSearch() {
+    const data = []
+    const queue = []
+    let node = this.root
+    queue.push(node)
+
+    while (queue.length) {
+      node = queue.shift()
+      data.push(node)
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+
+    return data
+  }
+
+  depthFirstPreOrderSearch() {
+    const data = []
+
+    function traverse(node) {
+      data.push(node)
+      if (node.left) traverse(node.left)
+      if (node.right) traverse(node.right)
+    }
+
+    traverse(this.root)
+
+    return data
+  }
+
+  depthFirstPostOrderSearch() {
+    const data = []
+
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      if (node.right) traverse(node.right)
+      data.push(node)
+    }
+
+    traverse(this.root)
+
+    return data
+  }
+
+  depthFirstInOrderSearch() {
+    const data = []
+
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      data.push(node.value)
+      if (node.right) traverse(node.right)
+    }
+
+    traverse(this.root)
+
+    return data
+  }
 }
 
 //        10
