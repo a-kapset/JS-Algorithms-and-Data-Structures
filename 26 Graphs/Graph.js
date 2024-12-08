4a4e1dc5c21072b380dedd84f2d5f176ad007ad2
Artsiom Kapset <artsiom.kapset@mmc.com>
2024-12-08 16:14:25 +0100

    graph traversal functions are add

--- 26 Graphs/Graph.js
+++ 26 Graphs/Graph.js
@@ -27,7 +27,79 @@ class Graph {
 
     delete this.adjacencyList[vertex]
   }
+
+  dfsRecursively(start) {
+    const result = []
+    const visited = {}
+    const adjacencyList = this.adjacencyList
+
+    function dfs(vertex) {
+      if (!vertex) return null
+      visited[vertex] = true
+      result.push(vertex)
+
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) return dfs(neighbor)
+      })
+    }
+
+    dfs(start)
+
+    return result
+  }
+
+  dfsIteratively(start) {
+    const stack = [start]
+    const result = []
+    const visited = {}
+    visited[start] = true
+    let currentVertex
+
+    while (stack.length) {
+      currentVertex = stack.pop()
+      result.push(currentVertex)
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true
+          stack.push(neighbor)
+        }
+      })
+    }
+
+    return result
+  }
+
+  bfsIteratively(start) {
+    const queue = [start]
+    const result = []
+    const visited = {}
+    visited[start] = true
+    let currentVertex
+
+    while (queue.length) {
+      currentVertex = queue.shift()
+      result.push(currentVertex)
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true
+          queue.push(neighbor)
+        }
+      })
+    }
+
+    return result
+  }
 }
+
+//         A
+//       /   \
+//      B     C
+//      |     |
+//      D --- E
+//       \   /
+//         F
 
 const gr = new Graph()
 
@@ -35,12 +107,14 @@ gr.addVertex('A')
 gr.addVertex('B')
 gr.addVertex('C')
 gr.addVertex('D')
+gr.addVertex('E')
+gr.addVertex('F')
 gr.addEdge('A', 'B')
 gr.addEdge('A', 'C')
-gr.addEdge('B', 'C')
-gr.addEdge('A', 'D')
-gr.addEdge('D', 'C')
+gr.addEdge('B', 'D')
+gr.addEdge('C', 'E')
+gr.addEdge('D', 'E')
+gr.addEdge('D', 'F')
+gr.addEdge('E', 'F')
 
-console.log(gr)
-gr.removeVertex('A')
-console.log(gr)
+console.log(gr.bfsIteratively('A'))
